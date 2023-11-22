429321793c2f3170e940c18b799f36691fea6ac4
Hitesh Saini <hitesh9694saini@gmail.com>
2023-11-22 18:11:29 +0530

    add change

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import Frame from "./Components/Frame"
 function App() {
   return (
     <>
-      <div className="flex h-screen justify-center items-center">
+      <div className="flex   h-screen justify-center items-center">
         <Frame />
       </div>
+
     </>
   )
 }
